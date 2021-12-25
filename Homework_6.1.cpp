f5c8f6e99a383bfceeca3e082ea5a014ec7022ab
UnnamedBread <91851637+UnnamedBread@users.noreply.github.com>
2021-12-26 01:02:43 +0300

    Add files via upload

---
+++ Homework_6.1.cpp
@@ -0,0 +1,79 @@
+﻿#include <iostream>
+#include <windows.h>
+#include <string>
+using namespace std;
+
+int random() {
+    
+    int a = (rand() % 100) +1;
+    return(a);
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    srand(time(0));
+    int n;
+    int m;
+    int min = 101;
+    int nmin;
+    int mmin;
+    cin >> n >> m;
+    int **array = new int *[n];
+    for (int i = 0; i < n; i++) {
+        array[i] = new int [m];
+    }
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            array[i][j] = random();
+            if (min > array[i][j]) {
+                min = array[i][j];
+                nmin = i;
+                mmin = j;
+            }
+        }
+    }
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cout << array[i][j] << " ";
+        }
+        cout << endl;
+    }
+    cout << endl;
+
+    for (int i = nmin; i < n - 1; i++) {
+        for (int j = 0; j < m; j++) {
+            array[i][j] = array[i + 1][j];
+
+        }
+    }
+    
+    for (int j = mmin; j < m - 1; j++) {
+        for (int i = 0; i < n; i++) {
+            array[i][j] = array[i][j + 1];
+
+        }
+    }
+    
+    for (int i = 0; i < n; i++) {
+        array[i][m - 1] = 0;
+    }
+    for (int i = 0; i < m; i++) {
+        array[n-1][i] = 0;
+    }
+
+    for (int i = 0; i < n-1; i++) {
+        for (int j = 0; j < m-1; j++) {
+            cout << array[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    for (int i = 0; i < n; i++) {
+        delete array[i];
+    }
+    delete array;
+
+    return(0);
+}
---
+++ Homework_6.2.cpp
@@ -0,0 +1,91 @@
+﻿#include <iostream>
+#include <windows.h>
+#include <string>
+using namespace std;
+
+int random() {
+    int a = (rand() % 100) + 1;
+    return(a);
+}
+void random_array(int** array, int n) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            array[i][j] = random();
+        }
+    }
+}
+
+void print_array(int** array, int n) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cout << array[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+void composition(int** array1, int** array2, int** array3, int n) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            for (int k = 0; k < n; k++) {
+                array3[i][j] += array1[i][k] * array2[k][j];
+            }
+        }
+    }
+}
+
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    srand(time(0));
+    int n;
+    cin >> n;
+
+    int** array1 = new int* [n];
+    for (int i = 0; i < n; i++) {
+        array1[i] = new int[n];
+    }
+    int** array2 = new int* [n];
+    for (int i = 0; i < n; i++) {
+        array2[i] = new int[n];
+    }
+
+    int** array3 = new int* [n];
+    for (int i = 0; i < n; i++) {
+        array3[i] = new int[n];
+    }
+
+    random_array(array1, n);
+
+    random_array(array2, n);
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            array3[i][j] = 0;
+        }
+    }
+    
+    composition(array1, array2, array3, n);
+
+    print_array(array1, n);
+    cout << endl;
+    print_array(array2, n);
+    cout << endl;
+    print_array(array3, n);
+
+    for (int i = 0; i < n; i++) {
+        delete array1[i];
+    }
+    delete array1;
+    for (int i = 0; i < n; i++) {
+        delete array2[i];
+    }
+    delete array2;
+
+    for (int i = 0; i < n; i++) {
+        delete array3[i];
+    }
+    delete array3;
+    return(0);
+}
